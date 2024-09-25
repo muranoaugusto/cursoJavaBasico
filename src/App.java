55be86f3ca27dca06e7749e6c9a7b8ea7bcc055f
Guilherme Augusto <muranoaugusto@yahoo.com.br>
2024-09-25 12:30:37 -0300

    Testando Case menu principal

--- src/App.java
+++ src/App.java
@@ -10,21 +10,72 @@ public class App {
 
         Scanner sc = new Scanner(System.in);       
        
-        String[] exercicios = {"[1]exe2901", "[2]exe2902", "[3]exe89EXE", "[4]exe8901", "[5]exe8902", "[6]exe8903", "[7]exe8904", "[8]exe8905", "[9]VerificadorDeIdade"};
-        
+        String[] exercicios = {"[1]-29", "[2]-36", "[3]exe-89", "[4]-Projeto_Paralelo"};
         String x = new String();
-
         boolean result = false;
-
         while (result == false) {
-        
         switch (x) {
             
             case "1":
-                System.out.println("Execucao Exercicio 29 - 01");
-                E29 NovaExecucaoE2901 = new E29();
-                System.out.println(NovaExecucaoE2901.exe1());
-                result = true;
+                System.out.println("Execucao Exercicios 29");
+
+                String[] exercicios29 = {"[1]exe2901", "[2]exe2902","[3]Voltar"};
+                String x1 = new String();
+                boolean result1 = false;
+                while (result1 == false) {
+                switch (x1) {
+                    
+                    case "1":
+                        System.out.println("Execucao Exercicio 29 - 01");
+                        E29 NovaExecucaoE2901 = new E29();
+                        System.out.println(NovaExecucaoE2901.exe1());
+                        result1 = true;
+                    break;
+        
+                    case "2":
+                        System.out.println("Execucao Exercicio 29 - 02");
+                        E29 NovaExecucaoE2902 = new E29();
+                        System.out.printf("%.4f%n",NovaExecucaoE2902.exe2());
+                        result1 = true;
+                    break;
+
+                    case "3":
+                    result = false;
+                    result1 = false;
+                    break;
+        
+                    default:
+                        System.out.println("Selecione uma opcao valida");
+                        System.out.println(Arrays.toString(exercicios));
+                        x1 = sc.nextLine();
+                        result1 = false;
+                    break;
+                    }
+                }
+
+
+            case "2":
+            System.out.println("Execucao Exercicios 36");
+
+            String[] exercicios36 = {"[1]Voltar"};
+                String x2 = new String();
+                boolean result2 = false;
+                while (result2 == false) {
+                switch (x2) {
+                    
+                    case "1":
+                    result = false;
+                    result2 = false;
+                    break;
+        
+                    default:
+                        System.out.println("Selecione uma opcao valida");
+                        System.out.println(Arrays.toString(exercicios));
+                        x2 = sc.nextLine();
+                        result2 = false;
+                    break;
+                    }
+                }
             break;
 
             case "2":

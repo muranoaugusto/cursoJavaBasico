4d74e24899d2ab8470a742a7c3b90d9be5b1ed96
Guilherme Augusto <muranoaugusto@yahoo.com.br>
2024-11-25 06:43:20 -0300

    inicio do exercicio 104

---
+++ src/projects/e104/E104.java
@@ -0,0 +1,41 @@
+package projects.e104;
+
+import java.util.Scanner;
+
+public class E104 {
+
+    public static void main(String[] args){
+
+    //public  void employeeRegister() {
+
+    Scanner sc = new Scanner(System.in);
+    
+    int linha, coluna;
+    
+    System.out.print("Quantas linhas ");
+
+    linha = sc.nextInt();
+
+    System.out.print("Quantas colunas ");
+
+    coluna = sc.nextInt();
+
+    int [][] exec = new int[linha][coluna];
+
+
+ /*    for (int i=0; i<exec.length; i++) {
+        for (int )
+
+
+    }
+ */
+
+
+    System.out.print(exec);
+
+    sc.close();
+
+    }
+    
+
+}
